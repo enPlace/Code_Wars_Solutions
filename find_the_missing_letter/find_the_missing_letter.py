a54ef89e57719f2f415b47e502e6b57ee0623b7a
enPlace <n.place@protonmail.com>
2021-01-23 07:22:16 -0300

    initial commit

---
+++ find_the_missing_letter/find_the_missing_letter.py
@@ -0,0 +1,18 @@
+
+
+import string
+alphabet = list(string.ascii_lowercase) + list(string.ascii_uppercase)
+
+
+
+
+def find_missing_letter(chars):
+	#find the missing letters in a string
+	for i in alphabet: 
+		if i not in chars and alphabet[alphabet.index(i)-1] in chars and alphabet[alphabet.index(i)+1] in chars: 
+			chars.insert(chars.index(alphabet[alphabet.index(i)+1]), i)
+			return i
+
+
+
+print(find_missing_letter(['O','Q','R','S']))
