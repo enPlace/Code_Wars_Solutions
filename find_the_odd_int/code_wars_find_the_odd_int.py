146fc76d6460c4f795fc55a74f4c1b05c6e0afdf
enPlace <n.place@protonmail.com>
2021-01-24 16:44:16 -0700

    adding test cases

--- find_the_odd_int/code_wars_find_the_odd_int.py
+++ find_the_odd_int/code_wars_find_the_odd_int.py
@@ -4,4 +4,10 @@ def find_it(seq):
     for i in setlist: 
     	if seq.count(i)%2 !=0:
     	    return i
-    	    
+
+
+print(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]))
+print(find_it ([1,1,2,-2,5,2,4,4,-1,-2,5]))
+print(find_it ([20,1,1,2,2,3,3,5,5,4,20,4,5]))
+print(find_it ([10]))
+print(find_it ([1,1,1,1,1,1,10,1,1,1,1]))
