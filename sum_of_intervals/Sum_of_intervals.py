d22507c5a59b76f06b268db9fafefecebb4853c2
enPlace <77813192+enPlace@users.noreply.github.com>
2021-02-01 07:56:32 -0700

    Update Sum_of_intervals.py

--- sum_of_intervals/Sum_of_intervals.py
+++ sum_of_intervals/Sum_of_intervals.py
@@ -1,5 +1,5 @@
 """Description:
-https://www.codewars.com/kata/52b7ed099cdc285c300001cd/solutions/python/me/best_practice
+https://www.codewars.com/kata/52b7ed099cdc285c300001cd
 
 Write a function called sum_of_intervals that accepts an array of intervals, and returns the sum of all the interval lengths. 
 Overlapping intervals should only be counted once.
