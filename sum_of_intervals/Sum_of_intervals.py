2767b048e3ae5ce2d52f044c47f1dcd34771a012
enPlace <77813192+enPlace@users.noreply.github.com>
2021-02-01 07:56:05 -0700

    Update Sum_of_intervals.py

--- sum_of_intervals/Sum_of_intervals.py
+++ sum_of_intervals/Sum_of_intervals.py
@@ -1,10 +1,12 @@
 """Description:
 https://www.codewars.com/kata/52b7ed099cdc285c300001cd/solutions/python/me/best_practice
 
-Write a function called sumIntervals/sum_intervals() that accepts an array of intervals, and returns the sum of all the interval lengths. Overlapping intervals should only be counted once.
-Intervals
+Write a function called sum_of_intervals that accepts an array of intervals, and returns the sum of all the interval lengths. 
+Overlapping intervals should only be counted once.
 
-Intervals are represented by a pair of integers in the form of an array. The first value of the interval will always be less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
+
+Intervals are represented by a pair of integers in the form of an array. The first value of the interval will always be less than the second value. 
+Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
 Overlapping Intervals
 
 """
