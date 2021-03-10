095ef0e9d40f69b214c238737c8dcaab081bbbe2
enPlace <n.place@protonmail.com>
2021-03-10 16:02:08 -0300

    solution

---
+++ Exes_and_Ohs/exes_and_ohs.js
@@ -0,0 +1,19 @@
+/*Check to see if a string has the same amount of 'x's and 'o's. 
+The method must return a boolean and be case insensitive. 
+The string can contain any char.*/
+
+function XO(str) {
+	xcount = 0; 
+	ocount = 0;
+   for (let i in str){
+   	if (str.toLowerCase()[i]=='x'){
+   		xcount++
+   	}if (str.toLowerCase()[i]== 'o'){
+   		ocount++
+   	}
+   }
+   return xcount === ocount
+}
+
+
+console.log(XO('Xo'))
