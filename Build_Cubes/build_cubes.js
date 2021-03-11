9dceae27e8bb32eea4c3ac4e2af2539fad4d856b
enPlace <n.place@protonmail.com>
2021-03-11 15:18:29 -0300

    js solution

--- Build_Cubes/build_cubes.js
+++ Build_Cubes/build_cubes.js
@@ -1,16 +1,12 @@
+
 function findNb(m) {
  	let sum = 0
  	for (i=1; i<m+1; i++){
 	sum +=i**3; 
-	if (sum == m){
-		return i
-	};
-	if (sum >m){
-		return -1
-	};
-
+	if (sum == m){return i}; 
+	if (sum >m){ return -1};
 };
 	return -1
 }
 
-console.log(findNb(1))
+console.log(findNb(784))
