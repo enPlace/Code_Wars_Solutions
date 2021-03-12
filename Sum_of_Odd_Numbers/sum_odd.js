965c732f2ad6abc2742b05bb3180a25e14febd75
enPlace <n.place@protonmail.com>
2021-03-12 14:27:32 -0300

    initial commit

--- Sum_of_Odd_Numbers/sum_odd.js
+++ Sum_of_Odd_Numbers/sum_odd.js
@@ -1,13 +1,16 @@
 function rowSumOddNumbers(n) {
 	let numcount = 0; //total numbers in odd array
 	for (i=1; i< n+1; i++){
-		numcount +=i
-	}; 
+		numcount +=i}; 
+
 	let oddArray = []; 
 	for (j=1; j< (numcount*2)+1; j+=2){
-		oddArray.push(j) //gives us our odd array length numcount
-	}; 
+		oddArray.push(j)}; //gives us our odd array length numcount
+
 	const reducer = (accumulator, currentValue)=>accumulator +currentValue; //sum function
 	return oddArray.slice(-n).reduce(reducer)  //returns sum of last n numbers of array
 };
+
 console.log(rowSumOddNumbers(3))
+
+
