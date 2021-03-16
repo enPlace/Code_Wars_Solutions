e5f4ae5e987dec1ce52df49decff301a27f8b303
enPlace <n.place@protonmail.com>
2021-03-16 14:52:44 -0300

    practice

---
+++ practice.js
@@ -0,0 +1,9 @@
+function camelCase(text){
+	let arr = text.replace(/-/g, "_").split("_")
+	camel= arr.shift()
+	for (i = 0; i< arr.length; i++){
+		camel += arr[i][0].toUpperCase() + arr[i].slice(1)
+	}
+	return camel 
+}
+console.log(camelCase("a-Cat_was-a_kitty-Before"))
