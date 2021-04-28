ca68cb69a5531916dc64708e3fa4ac7f85b01f8a
enPlace <n.place@protonmail.com>
2021-04-28 18:46:47 -0300

    cool-string solutions

--- cool-string/cool-string.js
+++ cool-string/cool-string.js
@@ -1,4 +1,11 @@
 
+function coolString2(s) {
+	//most concise 
+  return !/[A-Z]{2}|[a-z]{2}/.test(s) && /^[a-zA-Z]*$/.test(s)
+
+
+ }
+
 
 
 
@@ -12,7 +19,6 @@ function coolString(s) {
   for(let i = 1; i<s.length; i++){
   	if(caps.test(s[i]) && caps.test(s[i-1]) || lower.test(s[i]) && lower.test(s[i-1])){
   		adjacent = true
-
   	}
 
   }
@@ -26,25 +32,10 @@ console.log(coolString("HeA1"))
 function coolStringy(s) {
   let characters = /[!@#$%^&*(),.?":{}|<>0-9 ]/g
   let match = characters.test(s)
-  let caps = /[A-Z]{2}/
-  let lower = /[a-z]{2}/
-  let adjacent = caps.test(s)|| lower.test(s)
+  let adjacent = /[a-z]{2}/.test(s)|| /[A-Z]{2}/.test(s)
 
 
-  
   return(adjacent ==true || match == true) ?false : true
   
 }
 console.log(coolStringy("AA"))
-
-function coolString2(s) {
-
-  return !/[A-Z]{2}|[a-z]{2}/.test(s) && /^[a-zA-Z]*$/.test(s)
-
-
- }
-
-  console.log(coolString2("aAAb"))
-
-  alpha = /^[0-9]*$/
-  console.log(alpha.test("1234568"))
